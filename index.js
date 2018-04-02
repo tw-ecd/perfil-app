4c8eed6f28c28454b3408bef8fcbfe624094e488
Caio Souza <caiobsouza@hotmail.com>
2018-04-02 11:11:02 -0300

    [Caio] Changed fallback port to 5000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
