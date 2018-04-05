c2799e2540af9c9b2f3756f441c9cfb4734965ad
Wagner Palacio <palaciowagner@gmail.com>
2018-04-05 11:33:49 -0300

    [Wagner|Caio] Created tests for POST method

--- test/routes/person.test.js
+++ test/routes/person.test.js
@@ -1,18 +1,19 @@
-process.env.NODE_ENV = 'test';
 
-let mongoose = require("mongoose");
-let Person = require('../../routes/person');
-
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../../index');
-let should = chai.should();
+const mongoose = require("mongoose");
+const Person = require('../../routes/person');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../index');
+const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('People', () => {
-    
+let personObject = {
+    name: "teste",
+    email: "t@t.com"
+};
 
+describe('People', () => {
     describe('/GET person', () => {
         it('it should return all people on database', (done) => {
           chai.request(server)
@@ -20,6 +21,23 @@ describe('People', () => {
               .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('array');
+                done();
+              });
+        });
+    });
+
+    describe('/POST person', () => {
+        it('it should create a new person on database', (done) => {
+          chai.request(server)
+              .post('/person')
+              .send(personObject)
+              .end((err, res) => {
+                  res.should.have.status(201);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('success');
+                  res.body.should.have.property('data');
+                  res.body.should.have.property('message');
+                  res.body.success.should.be.true;
                 done();
               });
         });
