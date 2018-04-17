7452cca362a5609816bc6999755b1b975c6e66a7
Caio Souza <caiobsouza@hotmail.com>
2018-04-17 15:19:32 -0300

    Created service to handle browser local storage

---
+++ src/app/providers/local-storage.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LocalStorageService {
+
+  keys = {
+    HAS_DONE: 'SU-HASDONE',
+    USER_ID: 'SU-USERID',
+    CURRENT_QUESTION: 'SU-CURRENTQUESTION'
+  }
+
+  constructor() { }
+
+  read(key: string){
+    return localStorage.getItem(key);
+  }
+
+  write(key: string, value: string){
+    localStorage.setItem(key, value);
+  }
+
+  clear(){
+    localStorage.clear();
+  }
+
+}
