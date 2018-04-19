86ba49755c27c34f8482c8f744f2260881b4e449
Caio Souza <caiobsouza@hotmail.com>
2018-04-19 18:09:04 -0300

    Finished ResultService calculation to define the aura profile. Refactored tests. Changed service implementation to use ES6 class

--- services/result.service.js
+++ services/result.service.js
@@ -1,35 +1,51 @@
 
-module.exports = (person) => {
+module.exports = class ResultService {
 
-    this.trendAnswers = person.answers.filter(a => a.typeOf === 'TREND' && a.weight > 0);
-    this.profileAnswers = person.answers.filter(a => a.typeOf === 'PROFILE' && a.weight > 0);
+    constructor(person) {
+        this.trendAnswers = person.answers.filter(a => a.typeOf === 'TREND' && a.weight > 0);
+        this.profileAnswers = person.answers.filter(a => a.typeOf === 'PROFILE' && a.weight > 0);
 
-    return {
-        calculateProfile: () => {
-            const weigthSum = this.profileAnswers.reduce((sum, answer) => {
-                return sum + answer.weight;
-            }, 0);
+        this.profileMatrix = {
+            'EVO_INTER': ['Engenheira de Hábitos', 'Geradora de Interações'],
+            'RISE_PLAT': ['Criadora de Possibilidades', 'Formadora de Plataformas'],
+            'SEC_PRIV': ['Mestra dos Números', 'Guardiã dos Dados'],
+            'HUMAN_AUG': ['Realizadora de Possibilidades', 'Criadora do Novo'],
+            'RISE_ROBOT': ['Líder das Máquinas', 'Mestra de Robôs'],
+        };
+    }
 
-            const finalValue = this.profileAnswers.reduce((avg, answer) => {
-                return avg + (answer.value * answer.weight) / weigthSum;
-            }, 0);
+    calculateProfile() {
+        const weigthSum = this.profileAnswers.reduce((sum, answer) => {
+            return sum + answer.weight;
+        }, 0);
 
-            return Math.round(finalValue);
-        },
+        const finalValue = this.profileAnswers.reduce((avg, answer) => {
+            return avg + (answer.value * answer.weight) / weigthSum;
+        }, 0);
 
-        calculateTrend: () => {
-            let responses = {};
+        return Math.round(finalValue);
+    }
 
-            const occurences = this.trendAnswers.forEach(answer => {
-                if (!responses.hasOwnProperty(answer.value)) {
-                    responses[answer.value] = answer.weight;
-                } else {
-                    responses[answer.value] += answer.weight;
-                }
-            });
+    calculateTrend() {
+        let responses = {};
 
-            return Object.keys(responses)
-                .reduce((a, b) => responses[a] > responses[b] ? a : b);
-        }
-    };
+        this.trendAnswers.forEach(answer => {
+            if (!responses.hasOwnProperty(answer.value)) {
+                responses[answer.value] = answer.weight;
+            } else {
+                responses[answer.value] += answer.weight;
+            }
+        });
+
+        return Object.keys(responses)
+            .reduce((a, b) => responses[a] > responses[b] ? a : b);
+    }
+
+    calculateAuraProfile() {
+        const trend = this.calculateTrend();
+        const profile = this.calculateProfile();
+
+        return this.profileMatrix[trend][profile];
+    }
+
 };
--- test/services/result.service.test.js
+++ test/services/result.service.test.js
@@ -1,93 +1,101 @@
 const chai = require('chai');
+const sinon = require('sinon');
 const should = chai.should();
 const ResultService = require('../../services/result.service');
 
-let personTest = {
+let fixture = {
     "answers": [
         {
-            "_id": "5ad8a25b73067100147b4dc5",
-            "question": "Qual dessas características você considera mais relevante em uma líder?",
             "option": "Coragem",
             "value": 'SNKDSDKM',
             "weight": 0,
             "typeOf": "PROFILE"
         },
         {
-            "_id": "5ad8a25d73067100147b4dc6",
-            "question": "Qual a sua área de atuação?",
             "option": "Tecnologia",
             "value": 0.6,
             "weight": 0.5,
             "typeOf": "PROFILE"
         },
         {
-            "_id": "5ad8a25e73067100147b4dc7",
-            "question": "Quais são seus maiores desafios atualmente?",
             "option": "Responder com agilidade",
             "value": 0.4,
             "weight": 0.5,
             "typeOf": "PROFILE"
         },
         {
-            "_id": "5ad8a26073067100147b4dc8",
-            "question": "Diante dessas mudanças, qual o seu maior foco?",
             "option": "Aprender na prática",
             "value": 0.6,
             "weight": 0,
             "typeOf": "PROFILE"
         },
         {
-            "_id": "5ad8a26273067100147b4dc9",
-            "question": "Como você se sente em relação ao futuro?",
             "option": "Otimista",
             "value": 0,
             "weight": 0,
             "typeOf": "TREND"
         },
         {
-            "_id": "5ad8a26373067100147b4dca",
-            "question": "Com qual dessas tendências você mais se identifica? ",
             "option": "Crescimento das Plataformas",
             "value": 'EVO_INTER',
             "weight": 2,
             "typeOf": "TREND"
         },
         {
-            "_id": "5ad8a26473067100147b4dcb",
-            "question": "Qual dessas tecnologias produzirá o maior impacto sobre os negócios?",
             "option": "Assistentes de Voz",
             "value": 'SEC_PRIV',
             "weight": 1,
             "typeOf": "TREND"
         },
         {
-            "_id": "5ad8a26673067100147b4dcc",
-            "question": "O que você sente diante das transformações tecnológicas atuais?",
             "option": "Receio",
             "value": 3,
             "weight": 0,
             "typeOf": "TREND"
         }
-    ],
-    "_id": "LXZPTM",
-    "name": null,
-    "email": null,
-    "company": null,
-    "role": null,
-    "__v": 0
+    ]
 };
 
 describe('Result Service', () => {
     describe('Calculate profile with {[{v: 0.4, w: 0.5}, {v: 0.6, w: 0.5}]}', () => {
+
+        let service;
+
+        beforeEach(() => {
+            service = new ResultService(fixture);
+        });
+
         it('should return a weighted average of 1', (done) => {
-            const service = ResultService(personTest);
             service.calculateProfile().should.be.equal(1);
             done();
         });
 
         it('should return a weighted result of EVO_INTER', (done) => {
-            const service = ResultService(personTest);
             service.calculateTrend().should.be.equal('EVO_INTER');
+            done();
+        });
+
+        it('should return \'Geradora de Interações\' as aura profile', (done) => {
+            sinon.stub(service, 'calculateTrend').returns('EVO_INTER');
+            sinon.stub(service, 'calculateProfile').returns(1);
+
+            service.calculateAuraProfile().should.be.equal('Geradora de Interações');
+            done();
+        });
+
+        it('should return \'Criadora de Possibilidades\' as aura profile when get RISE_PLAT and Profile is 0', (done) => {
+            sinon.stub(service, 'calculateTrend').returns('RISE_PLAT');
+            sinon.stub(service, 'calculateProfile').returns(0);
+
+            service.calculateAuraProfile().should.be.equal('Criadora de Possibilidades');
+            done();
+        });
+        
+        it('should return \'Criadora do Novo\' as aura profile when get HUMAN_AUG and Profile is 1', (done) => {
+            sinon.stub(service, 'calculateTrend').returns('HUMAN_AUG');
+            sinon.stub(service, 'calculateProfile').returns(1);
+
+            service.calculateAuraProfile().should.be.equal('Criadora do Novo');
             done();
         });
     });
