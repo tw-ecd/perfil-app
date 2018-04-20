d9ca225f481d550ec6ccd5a9f6e18fdc782d1cc6
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-04-20 19:17:31 -0300

    result email first draft: missing share links

--- scripts/test_email.js
+++ scripts/test_email.js
@@ -1,46 +1,21 @@
 require('dotenv').load();
 const winston = require('winston');
-const AWS = require('aws-sdk');
-const fs = require('fs');
+const EmailService = require('../services/email.service.js');
 
-let user = { _id: 'HAS2J7' };
-
-AWS.config.update({ region: process.env.AWS_REGION });
-
-const sendIntroEmail = function() {
-    let introHtml = fs.readFileSync('./data/intro.css.html', 'utf8');
-    let introText = fs.readFileSync('./data/intro.txt', 'utf8');
-
-    introHtml = introHtml.replace('{{user._id}}', user._id);
-    introText = introText.replace('{{user._id}}', user._id);
-
-    const params = {
-        Destination: {
-            ToAddresses: [ 'thiago.hersan@gmail.com' ]
-        },
-        Message: {
-            Body: {
-                Html: {
-                    Charset: 'UTF-8',
-                    Data: introHtml
-                },
-                Text: {
-                    Charset: 'UTF-8',
-                    Data: introText
-                }
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Seu Perfil de Pessoa Inovadora'
-            }
-        },
-        Source: '"ThoughtWorks - Inove Com Coragem" <inovecomcoragem@thoughtworks.com>'
-    };
-
-    return new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+let user = {
+    _id: '14XFCX9',
+    email: 'thersan@thoughtworks.com',
+    image_url: 'https://s3.amazonaws.com/tw-su-auras/YP1QT1_JG77W0BB.jpg',
+    profile: 'Geradora de interações',
+    href: 'https://caiobsouza.github.io/su-auras/result/14XFCX9',
+    text: 'Você acredita que as novas formas de interação entre pessoas e máquinas vai transformar a maneira como vivemos e nos relacionamos, criando novas possibilidades de engajamento para as marcas. Assistentes de voz, ausência de telas e IoT são só algumas das tecnologias que possibilitam esse mundo novo que você quer criar. A ThoughtWorks pode te acompanhar nessa jornada, desenvolvendo softwares personalizados para que você continue inovando com coragem.',
+    avatar_url: ''
 };
 
-var promise = sendIntroEmail();
+let emailService = new EmailService(user);
+
+var promise = emailService.sendResultEmail();
+
 promise.then(
     function(data) {
         winston.log('info', data.MessageId);
--- services/email.service.js
+++ services/email.service.js
@@ -7,10 +7,13 @@ function EmailService(user) {
     this.introHtml = fs.readFileSync('./data/intro.css.html', 'utf8');
     this.introText = fs.readFileSync('./data/intro.txt', 'utf8');
 
-    this.introHtml = this.introHtml.replace('{{user._id}}', user._id);
-    this.introText = this.introText.replace('{{user._id}}', user._id);
+    this.resultHtml = fs.readFileSync('./data/result.css.html', 'utf8');
+    this.resultText = fs.readFileSync('./data/result.txt', 'utf8');
 
     this.sendIntroEmail = function() {
+        this.introHtml = this.introHtml.replace('{{user._id}}', user._id);
+        this.introText = this.introText.replace('{{user._id}}', user._id);
+
         const params = {
             Destination: {
                 ToAddresses: [ user.email ]
@@ -33,7 +36,41 @@ function EmailService(user) {
             },
             Source: '"ThoughtWorks - Inove Com Coragem" <inovecomcoragem@thoughtworks.com>'
         };
+        return new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+    };
 
+    this.sendResultEmail = function() {
+        this.resultText = this.resultText.replace('{{user.profile}}', user.profile);
+        this.resultText = this.resultText.replace('{{user.text}}', user.text);
+
+        this.resultHtml = this.resultHtml.replace(/{{user.profile}}/g, user.profile);
+        this.resultHtml = this.resultHtml.replace(/{{user.text}}/g, user.text);
+        this.resultHtml = this.resultHtml.replace(/{{user.href}}/g, user.href);
+        this.resultHtml = this.resultHtml.replace(/{{user.avatar_url}}/g, user.avatar_url);
+        this.resultHtml = this.resultHtml.replace(/{{user.image_url}}/g, user.image_url);
+
+        const params = {
+            Destination: {
+                ToAddresses: [ user.email ]
+            },
+            Message: {
+                Body: {
+                    Html: {
+                        Charset: 'UTF-8',
+                        Data: this.resultHtml
+                    },
+                    Text: {
+                        Charset: 'UTF-8',
+                        Data: this.resultText
+                    }
+                },
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: 'Resultado de Perfil de Pessoa Inovadora'
+                }
+            },
+            Source: '"ThoughtWorks - Inove Com Coragem" <inovecomcoragem@thoughtworks.com>'
+        };
         return new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
     };
 }
