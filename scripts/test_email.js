3d80a6688d734a5c6b8e674278efe2568964d0ed
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-04-20 23:33:56 -0300

    makes email test script reflect changes in email service

--- scripts/test_email.js
+++ scripts/test_email.js
@@ -2,14 +2,15 @@ require('dotenv').load();
 const winston = require('winston');
 const EmailService = require('../services/email.service.js');
 
-let user = {
+const user = {
     _id: '1AU15T2',
     email: 'thersan@thoughtworks.com',
     image_url: 'https://s3.amazonaws.com/tw-su-auras/YP1QT1_JG77W0BB.jpg',
-    profile: 'Geradora de interações'
+    profile: 'Geradora de Interações',
+    text: 'Você...'
 };
 
-let emailService = new EmailService(user);
+const emailService = new EmailService(user);
 
 /*
 var intro = emailService.sendIntroEmail();
