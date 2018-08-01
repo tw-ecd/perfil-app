f4c01017129d57408f7644933b18a163856c6a68
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-08-01 14:17:38 -0300

    enable auto login on photo app

--- src/app/app-globals.service.ts
+++ src/app/app-globals.service.ts
@@ -4,10 +4,20 @@ import { Injectable } from '@angular/core';
 export class AppGlobals {
 
   private api_url = 'https://auras-app.herokuapp.com';
+  private photo_url = 'https://inovecomcoragem.github.io/auras-photo/login';
+  private auto_photo_login = false;
 
   constructor() { }
 
   public get apiURL(): string {
     return this.api_url;
+  }
+
+  public get photoURL(): string {
+    return this.photo_url;
+  }
+
+  public get autoPhotoLogin(): boolean {
+    return this.auto_photo_login;
   }
 }
--- src/app/components/code/code.component.ts
+++ src/app/components/code/code.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { PersonService } from '../../providers/person.service';
 import { Person } from '../../models/person.model';
+import { AppGlobals } from '../../app-globals.service';
 
 @Component({
   selector: 'app-code',
@@ -10,13 +11,16 @@ import { Person } from '../../models/person.model';
   styleUrls: ['./code.component.scss']
 })
 export class CodeComponent implements OnInit {
-
+  private photoUrl: string = this.globals.photoURL;
+  private autoPhotoLogin: boolean = this.globals.autoPhotoLogin;
   id: string;
   person;
 
   constructor(private personService: PersonService,
                private activedRoute: ActivatedRoute,
-               private renderer: Renderer2) { }
+               private renderer: Renderer2,
+               private globals: AppGlobals
+              ) { }
 
   ngOnInit() {
     this.renderer.removeAttribute(document.body, 'class');
@@ -27,6 +31,9 @@ export class CodeComponent implements OnInit {
         this.personService.get(this.id).subscribe(
           result => {
             this.person = result;
+            if (this.autoPhotoLogin) {
+              window.location.replace(this.photoUrl + '/' + this.id);
+            }
           }, err => { this.id = 'NOTFOUND'; }
         );
       });
